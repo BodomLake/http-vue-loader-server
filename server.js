2fa9827f19c15d40d6386ab150a54847cbdc323d
BodomLake <792294021@qq.com>
2022-02-16 16:04:54 +0800

    http-vue-loader 工程初始化

---
+++ server.js
@@ -0,0 +1,29 @@
+/**
+ * 一个简单的nodejs服务器，负责输出index.html
+ * 本程序主要测试 vue工程的服务端解析
+ */
+const http = require("http"),
+  fs = require("fs"),
+  path = require("path"),
+  url = require("url");
+var root = path.resolve();
+// 创建服务器
+var sever = http.createServer(function (request, response) {
+  var pathname = url.parse(request.url).pathname;
+  var filepath = path.join(root, pathname);
+  // 获取文件状态
+  fs.stat(filepath, function (err, stats) {
+    if (err) {
+      // 发送404响应
+      response.writeHead(404);
+      response.end("404 Not Found.");
+    } else {
+      // 发送200响应
+      response.writeHead(200);
+      // response是一个writeStream对象，fs读取html后，可以用pipe方法直接写入
+      fs.createReadStream(filepath).pipe(response);
+    }
+  });
+});
+sever.listen(8090);
+console.log('Sever is running at http://127.0.0.1:8090/');
